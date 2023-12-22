fd4a3a6fada39a2a57902859617ec5267257a7e1
Yusers <you@example.com>
2023-12-22 12:23:23 +0700

    adding JPA Auditing

--- src/main/java/com/website/genie/entities/BaseEntity.java
+++ src/main/java/com/website/genie/entities/BaseEntity.java
@@ -1,21 +1,45 @@
 package com.website.genie.entities;
 
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
+    @CreatedDate
     private Date createDate;
 
     @Column
-    private String modifiedBy;
+    @CreatedBy
+    private Date createBy;
+
     @Column
+    @LastModifiedBy
+    private String modifiedBy;
+
+    @Column
+    @LastModifiedDate
     private Date modifiedDate;
+
+
+    public Date getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(Date createBy) {
+        this.createBy = createBy;
+    }
 
     public Long getId() {
         return id;
